21cc117daf1b121e37b3dc055f740e00c2ee391d
ArunimaBharati <87299793+ArunimaBharati@users.noreply.github.com>
2022-03-01 20:36:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,11 +7,17 @@ function setup()
 {
     canvas=createCanvas(400,400);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(400, 400);
+    video.hide();
+
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw()
 {
-
+image(video,  0, 0, 400, 400);
 }
 
 function take_snapshot()
@@ -19,3 +25,17 @@ function take_snapshot()
     save('myFilter.png');
 }
 
+function modelLoaded()
+{
+    console.log('PoseNet is initialized');
+}
+
+function gotPoses(results)
+{
+    if (results.length>0){
+        console.log(results);
+        console.log("nose x="+results[0].pose.nose.x);
+        console.log("nose y="+results[0].pose.nose.y);
+        
+    }
+}
